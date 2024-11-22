9d5095c4733b6a92be9b2b7d06e56d2837e59290
Mikhail Vega <mikhail.vega@yahoo.com>
2024-11-22 09:12:31 +0000

    update model for admin site

--- blog/admin.py
+++ blog/admin.py
@@ -7,5 +7,6 @@ admin.site.register(Tag)
 
 class PostAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("title",)}
+    list_display = ("slug", "published_at")
 
 admin.site.register(Post, PostAdmin)
