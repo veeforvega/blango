34312b43552fcde2fa3a89cec0859d382fa44378
Mikhail Vega <mikhailvega@Mikhails-MacBook-Pro.local>
2024-11-28 20:22:03 -0600

    custom template tags

--- blog/templatetags/blog_extras.py
+++ blog/templatetags/blog_extras.py
@@ -28,3 +28,20 @@ def author_details(author, current_user=None):
         suffix = ""
 
     return format_html("{}{}{}", prefix, name, suffix)
+
+@register.simple_tag
+def row(extra_classes=""):
+    return format_html('<div class="row {}">', extra_classes)
+
+@register.simple_tag
+def endrow():
+    return format_html("</div>")
+
+@register.simple_tag
+def col(extra_classes=""):
+    return format_html('<div class="col {}">', extra_classes)
+
+
+@register.simple_tag
+def endcol():
+    return format_html("</div>")
